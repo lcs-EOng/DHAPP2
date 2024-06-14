2052a62c55b146f71672e50fbcd6aaf186a04c91
Eason Ong <eong@lcs.on.ca>
2024-06-14 14:51:27 -0400

    Initial Commit

---
+++ DHAPP2/DHAPP2App.swift
@@ -0,0 +1,17 @@
+//
+//  DHAPP2App.swift
+//  DHAPP2
+//
+//  Created by Ong Eason on 14/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct DHAPP2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
